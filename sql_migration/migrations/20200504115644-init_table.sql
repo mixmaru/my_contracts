6998263b7eb4b2c38a3364d153a8f41810be7dd7
mixmaru <mix.touch777@gmail.com>
2020-05-05 16:11:28 +0900

    usersテーブル周りの定義

--- sql_migration/migrations/20200504115644-init_table.sql
+++ sql_migration/migrations/20200504115644-init_table.sql
@@ -2,9 +2,32 @@
 -- +migrate Up
 create table users
 (
-    id bigint,
-    user_type smallint
+    id bigserial not null
+        constraint users_pk
+            primary key
+);
+
+create table "users_individual"
+(
+    user_id bigserial not null
+        constraint users_individual_pk
+            primary key
+        constraint users_individual_users_id_fk
+            references users (id),
+    name text not null
+);
+
+create table users_corporation
+(
+    user_id bigserial not null
+        constraint users_corporation_pk
+            primary key
+        constraint users_corporation_users_id_fk
+            references users (id),
+    name text not null
 );
 
 -- +migrate Down
+DROP TABLE users_corporation;
+DROP TABLE users_individual;
 DROP TABLE users;
