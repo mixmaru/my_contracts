e6486687ace1c356ff9d15e935f3dc752d905b11
mixmaru <mix.touch777@gmail.com>
2020-05-06 16:22:22 +0900

    データ型調整

--- sql_migration/migrations/20200504115644-init_table.sql
+++ sql_migration/migrations/20200504115644-init_table.sql
@@ -2,14 +2,14 @@
 -- +migrate Up
 create table users
 (
-    id bigserial not null
+    id serial not null
         constraint users_pk
             primary key
 );
 
 create table "users_individual"
 (
-    user_id bigserial not null
+    user_id int not null
         constraint users_individual_pk
             primary key
         constraint users_individual_users_id_fk
@@ -19,7 +19,7 @@ create table "users_individual"
 
 create table users_corporation
 (
-    user_id bigserial not null
+    user_id int not null
         constraint users_corporation_pk
             primary key
         constraint users_corporation_users_id_fk
@@ -33,17 +33,17 @@ create table products
         constraint products_pk
             primary key,
     name text not null,
-    price int not null
+    price decimal not null
 );
 create unique index products_name_uindex
     on products (name);
 
 create table contracts
 (
-    id bigserial not null
+    id serial not null
         constraint contracts_pk
             primary key,
-    user_id bigint not null
+    user_id int not null
         constraint contracts_users_id_fk
             references users,
     product_id int not null
@@ -56,12 +56,12 @@ create table contract_updates
     id bigserial not null
         constraint contract_updates_pk
             primary key,
-    contract_id bigint not null
+    contract_id int not null
         constraint contract_updates_contracts_id_fk
             references contracts,
     update_num int not null,
-    start timestamp not null,
-    "end" timestamp not null
+    valid_from timestamp not null,
+    valid_to timestamp not null
 );
 create unique index contract_updates_contract_id_update_num_uindex
     on contract_updates (contract_id, update_num);
@@ -93,7 +93,7 @@ create unique index bill_details_bill_id_order_num_uindex
 
 create table discounts
 (
-    id bigserial not null
+    id serial not null
         constraint discounts_pk
             primary key,
     discount_type smallint not null,
@@ -106,19 +106,19 @@ create table discounts
 
 create table discount_individual
 (
-    discount_id bigint not null
+    discount_id int not null
         constraint discount_individual_pk
             primary key
         constraint discount_individual_discounts_id_fk
             references discounts,
-    user_id bigint not null
+    user_id int not null
         constraint discount_individual_users_id_fk
             references users
 );
 
 create table discount_products
 (
-    discount_id bigint not null
+    discount_id int not null
         constraint discount_products_pk
             primary key
         constraint discount_products_discounts_id_fk
@@ -130,12 +130,12 @@ create table discount_products
 
 create table discount_contracts
 (
-    discount_id bigint not null
+    discount_id int not null
         constraint discount_contracts_pk
             primary key
         constraint discount_contracts_discounts_id_fk
             references discounts,
-    contract_id bigint not null
+    contract_id int not null
         constraint discount_contracts_contracts_id_fk
             references contracts
 );
@@ -145,7 +145,7 @@ create table discount_applies
     id bigserial not null
         constraint discount_applies_pk
             primary key,
-    discount_id bigint not null
+    discount_id int not null
         constraint discount_applies_discounts_id_fk
             references discounts
 );
