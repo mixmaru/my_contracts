fdfa2d435d664e4533702f4d5b1d99a100388098
mixmaru <mix.touch777@gmail.com>
2020-05-10 15:40:28 +0900

    map用データに変換する処理実装

--- internal/domains/contracts/repositories/user/structures/user_individual.go
+++ internal/domains/contracts/repositories/user/structures/user_individual.go
@@ -1,10 +1,22 @@
 package structures
 
-import "time"
+import (
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities/user"
+	"time"
+)
 
 type UserIndividual struct {
 	UserId    int       `db:"user_id"`
 	Name      string    `db:"name"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
+}
+
+func LoadUserIndividual(entity *user.UserIndividual) *UserIndividual {
+	return &UserIndividual{
+		UserId:    entity.Id(),
+		Name:      entity.Name(),
+		CreatedAt: entity.CreatedAt(),
+		UpdatedAt: entity.UpdatedAt(),
+	}
 }
