578c5cbb4027149510ed7592ae7e9364c44c31d6
mixmaru <mix.touch777@gmail.com>
2020-05-11 22:38:34 +0900

    loadメソッド定義

--- internal/domains/contracts/entities/user/user_corporation_entity.go
+++ internal/domains/contracts/entities/user/user_corporation_entity.go
@@ -8,14 +8,22 @@ type UserCorporationEntity struct {
 	*UserEntity
 	contactPersonName string //担当者名
 	presidentName     string //社長名
-	createdAt         time.Time
-	updatedAt         time.Time
 }
 
 func NewUserCorporationEntity() *UserCorporationEntity {
 	return &UserCorporationEntity{
 		UserEntity: &UserEntity{},
 	}
+}
+
+func NewUserCorporationEntityWithData(id int, contractPersonName, presidentName string, createdAt, updatedAt time.Time) *UserCorporationEntity {
+	user := NewUserCorporationEntity()
+	user.id = id
+	user.SetContactPersonName(contractPersonName)
+	user.SetPresidentName(presidentName)
+	user.createdAt = createdAt
+	user.updatedAt = updatedAt
+	return user
 }
 
 func (u *UserCorporationEntity) SetContactPersonName(name string) {
--- internal/domains/contracts/entities/user/user_corporation_entity_test.go
+++ internal/domains/contracts/entities/user/user_corporation_entity_test.go
@@ -31,3 +31,21 @@ func TestUserCorporationEntity_NewUserCorporationEntity(t *testing.T) {
 	assert.Equal(t, time.Time{}, user.UpdatedAt())
 	assert.Equal(t, time.Time{}, user.CreatedAt())
 }
+
+func TestUserCorporationEntity_NewUserCorporationEntityWithData(t *testing.T) {
+	// インスタンス化
+	user := NewUserCorporationEntityWithData(
+		1,
+		"担当太郎",
+		"社長次郎",
+		time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+		time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+	)
+
+	// テスト
+	assert.Equal(t, 1, user.Id())
+	assert.Equal(t, "担当太郎", user.ContactPersonName())
+	assert.Equal(t, "社長次郎", user.PresidentName())
+	assert.Equal(t, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), user.CreatedAt())
+	assert.Equal(t, time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC), user.UpdatedAt())
+}
