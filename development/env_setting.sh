a7e9db654e18a8c9e2f8d3934b9711c53e6aca0d
mixmaru <mix.touch777@gmail.com>
2020-05-16 17:16:09 +0900

    ローカル開発環境用環境変数設ファイル追加

---
+++ development/env_setting.sh
@@ -0,0 +1,14 @@
+export DB_TEST_HOST=localhost
+export DB_TEST_NAME=my_contracts_test
+export DB_TEST_USER=postgres
+export DB_TEST_PASSWORD=password
+
+export DB_DEVELOPMENT_HOST=localhost
+export DB_DEVELOPMENT_NAME=my_contracts_development
+export DB_DEVELOPMENT_USER=postgres
+export DB_DEVELOPMENT_PASSWORD=password
+
+export DB_PRODUCTION_HOST=localhost
+export DB_PRODUCTION_NAME=my_contracts
+export DB_PRODUCTION_USER=postgres
+export DB_PRODUCTION_PASSWORD=password
