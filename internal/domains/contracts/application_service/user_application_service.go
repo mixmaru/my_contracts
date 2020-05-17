d91142e444b611070af5fd624d65215243e5e0dd
mixmaru <mix.touch777@gmail.com>
2020-05-17 15:39:51 +0900

    テスト追記

--- internal/domains/contracts/application_service/user_application_service.go
+++ internal/domains/contracts/application_service/user_application_service.go
@@ -1,6 +1,9 @@
 package application_service
 
-import "github.com/mixmaru/my_contracts/internal/domains/contracts/application_service/interfaces"
+import (
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/application_service/interfaces"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities/user"
+)
 
 type UserApplicationService struct {
 	userRepository interfaces.IUserRepository
@@ -15,5 +18,11 @@ func NewUserApplicationService(userRepository interfaces.IUserRepository) *UserA
 // 個人顧客を新規登録する
 // 成功時、userIDを返却する
 func (s *UserApplicationService) RegisterUserIndividual(name string) (int, error) {
+	// エンティティ作成
+	userEntity := user.NewUserIndividualEntity()
+	userEntity.SetName(name)
+
+	// リポジトリ使って保存
+	//repository := NewRepo
 	return 0, nil
 }
--- internal/domains/contracts/application_service/user_application_service_test.go
+++ internal/domains/contracts/application_service/user_application_service_test.go
@@ -3,6 +3,7 @@ package application_service
 import (
 	"github.com/golang/mock/gomock"
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/application_service/interfaces/mock_interfaces"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities/user"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -23,6 +24,14 @@ func TestUserApplicationService_RegisterUserIndividual(t *testing.T) {
 	defer ctrl.Finish()
 	userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
 
+	//// チェック用にメソッドを設定。
+	//// 来る想定の引数でメソッドが実行されるかチェックする
+	// [個人太郎]がセットされたuserEntityとトランザクション(nilではない)が渡されて1回だけ実行されるはず
+	userEntity := user.NewUserIndividualEntity()
+	userEntity.SetName("個人太郎")
+	userRepositoryMock.EXPECT().SaveUserIndividual(gomock.Eq(userEntity), gomock.Not(nil)).Return(nil).Times(1)
+
+	// モックを渡してインスタンス化
 	userApp := NewUserApplicationService(userRepositoryMock)
 
 	userId, err := userApp.RegisterUserIndividual("個人太郎")
