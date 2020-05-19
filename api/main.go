42e24b5c84d67b34fbbcc270ee03570f5a0b92b8
mixmaru <mix.touch777@gmail.com>
2020-05-19 21:39:54 +0900

    apiアクセスで個人顧客の新規登録ok

--- api/main.go
+++ api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/application_service"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/user"
 	"net/http"
 )
 
@@ -15,13 +18,28 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
+	e.POST("/individual_users", saveIndividualUser)
+	//e.GET("/users/:id", getUser)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+// e.POST("/individual_users", saveUser)
+// params:
+// name string 個人顧客名
+// curl -F "name=個人　太郎" http://localhost:1323/individual_users
+func saveIndividualUser(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	userRepository := &user.Repository{}
+	userAppService := application_service.NewUserApplicationService(userRepository)
+	user, err := userAppService.RegisterUserIndividual(name)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "処理に失敗しました。")
+	}
+
+	return c.String(http.StatusOK, fmt.Sprintf("登録成功。%+v", user))
 }
