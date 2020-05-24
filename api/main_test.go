06acca52782a3e751f2fab4e736b51feec39b96c
mixmaru <mix.touch777@gmail.com>
2020-05-24 15:11:02 +0900

    取得apiできた

--- api/main_test.go
+++ api/main_test.go
@@ -7,13 +7,16 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strconv"
 	"strings"
 	"testing"
 )
 
-func TestMain_saveIndividualUser(t *testing.T) {
+func TestMain_saveIndividualUser_getIndividualUser(t *testing.T) {
 	t.Run("正常系", func(t *testing.T) {
 		router := newRouter()
+
+		//////// データ登録テスト
 
 		// リクエストパラメータ作成
 		body := url.Values{}
@@ -35,6 +38,22 @@ func TestMain_saveIndividualUser(t *testing.T) {
 
 		assert.Equal(t, "個人　太郎", registeredUser.Name)
 
+		///////// データ取得テスト
+		// リクエスト実行
+		registeredId := strconv.Itoa(registeredUser.Id)
+		req = httptest.NewRequest("GET", "/individual_users/"+registeredId, strings.NewReader(body.Encode()))
+		rec = httptest.NewRecorder()
+		router.ServeHTTP(rec, req)
+
+		// 検証
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		var loadedUser data_transfer_objects.UserIndividualDto
+		// jsonパース
+		err = json.Unmarshal(rec.Body.Bytes(), &loadedUser)
+		assert.NoError(t, err)
+
+		assert.Equal(t, registeredUser, loadedUser)
 	})
 
 	t.Run("バリデーションエラー", func(t *testing.T) {
@@ -59,3 +78,30 @@ func TestMain_saveIndividualUser(t *testing.T) {
 		assert.NoError(t, err)
 	})
 }
+
+//
+//func TestMain_getIndividualUser(t *testing.T) {
+//	t.Run("正常系", func(t *testing.T) {
+//		router := newRouter()
+//
+//		// リクエストパラメータ作成
+//		body := url.Values{}
+//		//body.Set("name", "個人　太郎")
+//
+//		// リクエスト実行
+//		req := httptest.NewRequest("GET", "/individual_users/1", strings.NewReader(body.Encode()))
+//		rec := httptest.NewRecorder()
+//		router.ServeHTTP(rec, req)
+//
+//		// 検証
+//		assert.Equal(t, http.StatusOK, rec.Code)
+//
+//		// jsonパース
+//		var registeredUser data_transfer_objects.UserIndividualDto
+//		err := json.Unmarshal(rec.Body.Bytes(), &registeredUser)
+//		assert.NoError(t, err)
+//
+//		assert.Equal(t, "個人　太郎", registeredUser.Name)
+//
+//	})
+//}
