434302a1a8221f7668cc62c089bc46a0ca56b30c
mixmaru <mix.touch777@gmail.com>
2020-05-31 14:18:59 +0900

    一旦保存

--- internal/domains/contracts/entities/user/values/contact_person_name_value.go
+++ internal/domains/contracts/entities/user/values/contact_person_name_value.go
@@ -1,19 +1,26 @@
 package values
 
+import (
+	plain_err "errors"
+	"fmt"
+	"github.com/pkg/errors"
+	"strings"
+)
+
 // Name値オブジェクト
 type ContactPersonNameValue struct {
 	value string
 }
 
 func NewContactPersonNameValue(value string) (ContactPersonNameValue, error) {
-	//validateErrors := NameValidate(value)
-	//if len(validateErrors) > 0 {
-	//	var msgs []string
-	//	for _, msg := range validateErrors {
-	//		msgs = append(msgs, msg.Error())
-	//	}
-	//	return NameValue{}, errors.New(fmt.Sprintf("Nameバリデーションエラー。%v", strings.Join(msgs, ", ")))
-	//}
+	validateErrors := ContactPersonNameValidate(value)
+	if len(validateErrors) > 0 {
+		var msgs []string
+		for _, msg := range validateErrors {
+			msgs = append(msgs, msg.Error())
+		}
+		return ContactPersonNameValue{}, errors.New(fmt.Sprintf("Nameバリデーションエラー。%v", strings.Join(msgs, ", ")))
+	}
 	return ContactPersonNameValue{
 		value: value,
 	}, nil
@@ -23,18 +30,18 @@ func (v *ContactPersonNameValue) Value() string {
 	return v.value
 }
 
-//func NameValidate(name string) []error {
-//	var validErrors []error
-//	if isEmpty(name) {
-//		validErrors = append(validErrors, EmptyValidError{plain_err.New("nameが空です")})
-//	}
-//	if isOverLength(name) {
-//		validErrors = append(validErrors, OverLengthValidError{plain_err.New(fmt.Sprintf("nameが50文字より多いです。name: %v", name))})
-//	}
-//
-//	return validErrors
-//}
-//
+func ContactPersonNameValidate(name string) []error {
+	var validErrors []error
+	if isEmpty(name) {
+		validErrors = append(validErrors, EmptyValidError{plain_err.New("nameが空です")})
+	}
+	if isOverLength(name) {
+		validErrors = append(validErrors, OverLengthValidError{plain_err.New(fmt.Sprintf("nameが50文字より多いです。name: %v", name))})
+	}
+
+	return validErrors
+}
+
 //func isEmpty(name string) bool {
 //	if utf8.RuneCountInString(name) == 0 {
 //		return true
--- internal/domains/contracts/entities/user/values/contact_person_name_value_test.go
+++ internal/domains/contracts/entities/user/values/contact_person_name_value_test.go
@@ -12,12 +12,12 @@ func TestContactPersonNameValue_NewContactPersonNameValue(t *testing.T) {
 		assert.Equal(t, ContactPersonNameValue{"担当者名"}, name)
 	})
 
-	//t.Run("名前が空文字だった時", func(t *testing.T) {
-	//	name, err := NewNameValue("")
-	//	assert.Error(t, err)
-	//	assert.Equal(t, NameValue{}, name)
-	//})
-	//
+	t.Run("名前が空文字だった時", func(t *testing.T) {
+		name, err := NewContactPersonNameValue("")
+		assert.Error(t, err)
+		assert.Equal(t, ContactPersonNameValue{}, name)
+	})
+
 	//t.Run("名前が50文字を超えていた時", func(t *testing.T) {
 	//	name, err := NewNameValue("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
 	//	assert.Error(t, err)
@@ -31,27 +31,27 @@ func TestContactPersonNameValue_NewContactPersonNameValue(t *testing.T) {
 	//})
 }
 
-//func TestNameValue_NameValidate(t *testing.T) {
-//	t.Run("正常系", func(t *testing.T) {
-//		errs := NameValidate("個人顧客名")
-//		assert.Len(t, errs, 0)
-//	})
-//
-//	t.Run("名前が空文字だった時", func(t *testing.T) {
-//		errs := NameValidate("")
-//		assert.Len(t, errs, 1)
-//		assert.EqualError(t, errs[0], "nameが空です")
-//	})
-//
-//	t.Run("名前が50文字を超えていた時", func(t *testing.T) {
-//		errs := NameValidate("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
-//		assert.Len(t, errs, 1)
-//		assert.EqualError(t, errs[0], "nameが50文字より多いです。name: 0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
-//	})
-//
-//	t.Run("名前が50文字だった時", func(t *testing.T) {
-//		errs := NameValidate("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789")
-//		assert.Len(t, errs, 0)
-//	})
-//
-//}
+func TestNameValue_ContactPersonNameValidate(t *testing.T) {
+	t.Run("正常系", func(t *testing.T) {
+		errs := ContactPersonNameValidate("担当者名")
+		assert.Len(t, errs, 0)
+	})
+
+	t.Run("名前が空文字だった時", func(t *testing.T) {
+		errs := ContactPersonNameValidate("")
+		assert.Len(t, errs, 1)
+		assert.EqualError(t, errs[0], "nameが空です")
+	})
+
+	t.Run("名前が50文字を超えていた時", func(t *testing.T) {
+		errs := ContactPersonNameValidate("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
+		assert.Len(t, errs, 1)
+		assert.EqualError(t, errs[0], "nameが50文字より多いです。name: 0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
+	})
+
+	t.Run("名前が50文字だった時", func(t *testing.T) {
+		errs := ContactPersonNameValidate("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789")
+		assert.Len(t, errs, 0)
+	})
+
+}
