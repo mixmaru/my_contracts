09caa2b94643a93e613554e33a2baafd197cdd14
mixmaru <mix.touch777@gmail.com>
2020-05-31 12:09:51 +0900

    テスト追加

--- api/main.go
+++ api/main.go
@@ -29,6 +29,8 @@ func newRouter() *echo.Echo {
 	e.POST("/individual_users/", saveIndividualUser)
 	// 個人顧客情報取得
 	e.GET("/individual_users/:id", getIndividualUser)
+	// 個人顧客新規登録
+	e.POST("/corporation_users/", saveCorporationUser)
 	//e.GET("/users/:id", getUser)
 	//e.PUT("/users/:id", updateUser)
 	//e.DELETE("/users/:id", deleteUser)
@@ -100,4 +102,38 @@ func getIndividualUser(c echo.Context) error {
 
 	// 返却
 	return c.JSON(http.StatusOK, user)
+}
+
+// 法人顧客新規登録
+// params:
+// contact_name string 担当者名
+// president_name string 社長名
+// curl -F "contact_name=担当　太郎" -F "president_name=社長　太郎" http://localhost:1323/corporation_users/
+func saveCorporationUser(c echo.Context) error {
+	//logger, err := my_logger.GetLogger()
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Get name and email
+	//name := c.FormValue("name")
+	//userAppService := application_service.NewUserApplicationService()
+	//user, validErrs, err := userAppService.RegisterUserIndividual(name)
+	//if err != nil {
+	//	logger.Sugar().Errorw("個人顧客データ登録に失敗。", "name", name, "err", err)
+	//	c.Error(err)
+	//	return err
+	//}
+	//if len(validErrs) > 0 {
+	//	validMessages := map[string][]string{
+	//		"name": []string{},
+	//	}
+	//	for _, err := range validErrs {
+	//		validMessages["name"] = append(validMessages["name"], err.Error())
+	//	}
+	//	return c.JSON(http.StatusBadRequest, validMessages)
+	//}
+	//
+	//return c.JSON(http.StatusCreated, user)
+	return nil
 }
--- api/main_test.go
+++ api/main_test.go
@@ -100,3 +100,72 @@ func TestMain_getIndividualUser(t *testing.T) {
 		assert.Equal(t, http.StatusNotFound, rec.Code)
 	})
 }
+
+func TestMain_saveCorporationUser(t *testing.T) {
+	t.Run("正常系", func(t *testing.T) {
+		router := newRouter()
+
+		//////// データ登録テスト
+
+		// リクエストパラメータ作成
+		body := url.Values{}
+		body.Set("contact_name", "担当　太郎")
+		body.Set("president_name", "社長　太郎")
+
+		// リクエスト実行
+		req := httptest.NewRequest("POST", "/corporation_users/", strings.NewReader(body.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded") //formからの入力ということを指定してるっぽい
+		rec := httptest.NewRecorder()
+		router.ServeHTTP(rec, req)
+
+		// 検証
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		// jsonパース
+		var registeredUser data_transfer_objects.UserCorporationDto
+		err := json.Unmarshal(rec.Body.Bytes(), &registeredUser)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "担当　太郎", registeredUser.ContactPersonName)
+		assert.Equal(t, "社長　太郎", registeredUser.PresidentName)
+
+		///////// データ取得テスト
+		// リクエスト実行
+		registeredId := strconv.Itoa(registeredUser.Id)
+		req = httptest.NewRequest("GET", "/individual_users/"+registeredId, nil)
+		rec = httptest.NewRecorder()
+		router.ServeHTTP(rec, req)
+
+		// 検証
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		var loadedUser data_transfer_objects.UserIndividualDto
+		// jsonパース
+		err = json.Unmarshal(rec.Body.Bytes(), &loadedUser)
+		assert.NoError(t, err)
+
+		assert.Equal(t, registeredUser, loadedUser)
+	})
+
+	//t.Run("バリデーションエラー", func(t *testing.T) {
+	//	router := newRouter()
+	//
+	//	// リクエストパラメータ作成
+	//	body := url.Values{}
+	//	body.Set("name", "")
+	//
+	//	// リクエスト実行
+	//	req := httptest.NewRequest("POST", "/individual_users/", strings.NewReader(body.Encode()))
+	//	req.Header.Set("Content-Type", "application/x-www-form-urlencoded") //formからの入力ということを指定してるっぽい
+	//	rec := httptest.NewRecorder()
+	//	router.ServeHTTP(rec, req)
+	//
+	//	// 検証
+	//	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	//
+	//	// jsonパース
+	//	var validMessages map[string][]string
+	//	err := json.Unmarshal(rec.Body.Bytes(), &validMessages)
+	//	assert.NoError(t, err)
+	//})
+}
