b1b43c6ece17fd8dbd3030d930c96a1aeb54adf5
mixmaru <mix.touch777@gmail.com>
2020-06-13 18:22:02 +0900

    保存だけできるようになった

--- internal/domains/contracts/repositories/product/product_repository.go
+++ internal/domains/contracts/repositories/product/product_repository.go
@@ -1,9 +1,9 @@
 package product
 
 import (
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities"
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/db_connection"
 	tables2 "github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/tables"
-	"github.com/mixmaru/my_contracts/internal/lib/decimal"
 	"github.com/pkg/errors"
 	"gopkg.in/gorp.v2"
 )
@@ -11,18 +11,27 @@ import (
 type ProductRepository struct {
 }
 
-// 商品エンティティを保存する
-func (r *ProductRepository) Save(productEntity interface{}, transaction *gorp.Transaction) (interface{}, error) {
+// 商品エンティティを新規保存する
+func (r *ProductRepository) Save(productEntity *entities.ProductEntity, transaction *gorp.Transaction) (*entities.ProductEntity, error) {
+	// db接続
 	conn, err := db_connection.GetConnectionIfNotTransaction(transaction)
 	if err != nil {
 		return nil, err
 	}
 	defer db_connection.CloseConnectionIfNotTransaction(conn)
 
-	productRecord := tables2.ProductRecord{Name: "商品名4", Price: decimal.NewFromFloat(1000)}
+	// recordオブジェクトに詰め替え
+	productRecord := tables2.ProductRecord{
+		Name:  productEntity.Name(),
+		Price: productEntity.Price(),
+	}
+
+	// 新規保存実行
 	err = conn.Insert(&productRecord)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
+
+	// エンティティに詰め直し
 	return nil, nil
 }
--- internal/domains/contracts/repositories/product/product_repository_test.go
+++ internal/domains/contracts/repositories/product/product_repository_test.go
@@ -2,15 +2,22 @@ package product
 
 import (
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/db_connection"
 	"github.com/mixmaru/my_contracts/internal/lib/decimal"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestProductRepository_Save(t *testing.T) {
+	// テーブル事前削除
+	db, err := db_connection.GetConnection()
+	assert.NoError(t, err)
+	_, err = db.Exec("truncate table products cascade")
+	assert.NoError(t, err)
+
 	r := ProductRepository{}
 	productEntity := entities.NewProductEntity("商品名", decimal.NewFromFloat(1000))
-	_, err := r.Save(productEntity, nil)
+	_, err = r.Save(productEntity, nil)
 	assert.NoError(t, err)
 	//assert.NotEqual(t, 0, productEntity.Id())
 }
