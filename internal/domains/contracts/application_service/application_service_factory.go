671c31e6d6a70247d666d236ed721044cd020165
mixmaru <mix.touch777@gmail.com>
2020-06-14 15:19:24 +0900

    application_service_factory追加

--- internal/domains/contracts/application_service/application_service_factory.go
+++ internal/domains/contracts/application_service/application_service_factory.go
@@ -2,13 +2,24 @@ package application_service
 
 import (
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/application_service/interfaces"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/product"
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/user"
 )
 
+// UserApplicationService
 func NewUserApplicationService() *UserApplicationService {
 	return &UserApplicationService{userRepository: &user.UserRepository{}}
 }
 
 func NewUserApplicationServiceWithMock(userRepository interfaces.IUserRepository) *UserApplicationService {
 	return &UserApplicationService{userRepository: userRepository}
+}
+
+// ProductApplicationService
+func NewProductApplicationService() *ProductApplicationService {
+	return &ProductApplicationService{productRepository: &product.ProductRepository{}}
+}
+
+func NewProductApplicationServiceWithMock(prodcutRepository interfaces.IProductRepository) *ProductApplicationService {
+	return &ProductApplicationService{productRepository: prodcutRepository}
 }
