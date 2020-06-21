f9ea9d6b1d02389c331f16fd12b5463c9d767640
mixmaru <mix.touch777@gmail.com>
2020-06-21 22:17:37 +0900

    SetDataToEntityを実装

--- internal/domains/contracts/repositories/tables/user_corporation_view.go
+++ internal/domains/contracts/repositories/tables/user_corporation_view.go
@@ -1,7 +1,26 @@
 package tables
 
+import (
+	"fmt"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities"
+	"github.com/pkg/errors"
+	"reflect"
+)
+
 type UserCorporationView struct {
 	UserRecord
 	ContactPersonName string `db:"contact_person_name"`
 	PresidentName     string `db:"president_name"`
+}
+
+func (u *UserCorporationView) SetDataToEntity(entity interface{}) error {
+	userCorprationEntity, ok := entity.(*entities.UserCorporationEntity)
+	if !ok {
+		return errors.New(fmt.Sprintf("entityが*entities.UserCorporationEntityではない。%v", reflect.TypeOf(entity)))
+	}
+	err := userCorprationEntity.LoadData(u.Id, u.ContactPersonName, u.PresidentName, u.CreatedAt, u.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
 }
