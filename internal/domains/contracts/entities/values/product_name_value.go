7c312a181b7995b289521788a785cee335ad7151
mixmaru <mix.touch777@gmail.com>
2020-06-25 22:37:07 +0900

    product_name_valueオブジェクト作成

---
+++ internal/domains/contracts/entities/values/product_name_value.go
@@ -0,0 +1,46 @@
+package values
+
+import (
+	plain_err "errors"
+	"fmt"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities/values/validators"
+	"github.com/pkg/errors"
+	"strings"
+)
+
+const ProductNameMaxLength = 50
+
+// Name値オブジェクト
+type ProductNameValue struct {
+	value string
+}
+
+func NewProductNameValue(value string) (ProductNameValue, error) {
+	validateErrors := ProductNameValidate(value)
+	if len(validateErrors) > 0 {
+		var msgs []string
+		for _, msg := range validateErrors {
+			msgs = append(msgs, msg.Error())
+		}
+		return ProductNameValue{}, errors.New(fmt.Sprintf("ProductNameバリデーションエラー。%v", strings.Join(msgs, ", ")))
+	}
+	return ProductNameValue{
+		value: value,
+	}, nil
+}
+
+func (v *ProductNameValue) Value() string {
+	return v.value
+}
+
+func ProductNameValidate(name string) []error {
+	var validErrors []error
+	if validators.IsEmptyString(name) {
+		validErrors = append(validErrors, validators.NewEmptyValidError(plain_err.New("空です")))
+	}
+	if validators.IsOverLengthString(name, ProductNameMaxLength) {
+		validErrors = append(validErrors, validators.NewOverLengthValidError(plain_err.New(fmt.Sprintf("%v文字より多いです", NameMaxLength))))
+	}
+
+	return validErrors
+}
---
+++ internal/domains/contracts/entities/values/product_name_value_test.go
@@ -0,0 +1,56 @@
+package values
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestProductNameValue_NewProductNameValue(t *testing.T) {
+	t.Run("正常系", func(t *testing.T) {
+		name, err := NewProductNameValue("商品名")
+		assert.NoError(t, err)
+		assert.Equal(t, ProductNameValue{"商品名"}, name)
+	})
+
+	t.Run("名前が空文字だった時", func(t *testing.T) {
+		name, err := NewProductNameValue("")
+		assert.Error(t, err)
+		assert.Equal(t, ProductNameValue{}, name)
+	})
+
+	t.Run("名前が50文字を超えていた時", func(t *testing.T) {
+		name, err := NewProductNameValue("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
+		assert.Error(t, err)
+		assert.Equal(t, ProductNameValue{}, name)
+	})
+
+	t.Run("名前が50文字だった時", func(t *testing.T) {
+		name, err := NewProductNameValue("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789")
+		assert.NoError(t, err)
+		assert.Equal(t, ProductNameValue{"0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789"}, name)
+	})
+}
+
+func TestProductNameValue_ProductNameValidate(t *testing.T) {
+	t.Run("正常系", func(t *testing.T) {
+		errs := ProductNameValidate("商品名")
+		assert.Len(t, errs, 0)
+	})
+
+	t.Run("名前が空文字だった時", func(t *testing.T) {
+		errs := ProductNameValidate("")
+		assert.Len(t, errs, 1)
+		assert.EqualError(t, errs[0], "空です")
+	})
+
+	t.Run("名前が50文字を超えていた時", func(t *testing.T) {
+		errs := ProductNameValidate("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789a")
+		assert.Len(t, errs, 1)
+		assert.EqualError(t, errs[0], "50文字より多いです")
+	})
+
+	t.Run("名前が50文字だった時", func(t *testing.T) {
+		errs := ProductNameValidate("0123456789０１２３４５６７８９0123456789０１２３４５６７８９0123456789")
+		assert.Len(t, errs, 0)
+	})
+}
