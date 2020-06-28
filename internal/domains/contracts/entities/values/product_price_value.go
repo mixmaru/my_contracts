a8545acf11130c8bb51bab27738f6a1e7254b18d
mixmaru <mix.touch777@gmail.com>
2020-06-28 15:31:17 +0900

    valueオブジェクトにバリデーション追加ok

--- internal/domains/contracts/entities/values/product_price_value.go
+++ internal/domains/contracts/entities/values/product_price_value.go
@@ -1,7 +1,9 @@
 package values
 
 import (
+	plain_err "errors"
 	"fmt"
+	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities/values/validators"
 	"github.com/mixmaru/my_contracts/internal/lib/decimal"
 	"github.com/pkg/errors"
 	"strings"
@@ -13,7 +15,11 @@ type ProductPriceValue struct {
 }
 
 func NewProductPriceValue(value string) (ProductPriceValue, error) {
-	validateErrors := ProductPriceValidate(value)
+	validateErrors, err := ProductPriceValidate(value)
+	if err != nil {
+		return ProductPriceValue{}, err
+	}
+
 	if len(validateErrors) > 0 {
 		var msgs []string
 		for _, msg := range validateErrors {
@@ -35,15 +41,27 @@ func (v *ProductPriceValue) Value() decimal.Decimal {
 	return v.value
 }
 
-func ProductPriceValidate(price string) []error {
-	var validErrors []error
-	//
-	//if validators.IsEmptyString(name) {
-	//	validErrors = append(validErrors, validators.NewEmptyValidError(plain_err.New("空です")))
-	//}
-	//if validators.IsOverLengthString(name, NameMaxLength) {
-	//	validErrors = append(validErrors, validators.NewOverLengthValidError(plain_err.New(fmt.Sprintf("%v文字より多いです", NameMaxLength))))
-	//}
+func ProductPriceValidate(price string) (validErrors []error, err error) {
+	// 空文字チェック
+	if validators.IsEmptyString(price) {
+		validErrors = append(validErrors, validators.NewEmptyStringValidError(plain_err.New("空です")))
+		return validErrors, nil
+	}
 
-	return validErrors
+	// 数値チェック
+	if !validators.IsNumericString(price) {
+		validErrors = append(validErrors, validators.NewNumericStringValidError(plain_err.New("数値ではありません")))
+		return validErrors, nil
+	}
+
+	// 正値チェック
+	priceDecimal, err := decimal.NewFromString(price)
+	if err != nil {
+		return nil, err
+	}
+	if priceDecimal.IsNegative() {
+		validErrors = append(validErrors, validators.NewNegativeValidError(plain_err.New("マイナスの数値です")))
+	}
+
+	return validErrors, nil
 }
--- internal/domains/contracts/entities/values/product_price_value_test.go
+++ internal/domains/contracts/entities/values/product_price_value_test.go
@@ -15,14 +15,12 @@ func TestProductPriceValue_NewNameValue(t *testing.T) {
 	})
 
 	t.Run("priceが空文字だった時", func(t *testing.T) {
-		t.Skip()
 		product, err := NewProductPriceValue("")
 		assert.Error(t, err)
 		assert.Equal(t, ProductPriceValue{}, product)
 	})
 
 	t.Run("priceがマイナスだったとき", func(t *testing.T) {
-		t.Skip()
 		product, err := NewProductPriceValue("-1000.01")
 		assert.Error(t, err)
 		assert.Equal(t, ProductPriceValue{}, product)
@@ -30,21 +28,30 @@ func TestProductPriceValue_NewNameValue(t *testing.T) {
 }
 
 func TestProductPriceValue_ProductPriceValidate(t *testing.T) {
-	t.Skip()
 	t.Run("正常系", func(t *testing.T) {
-		errs := ProductPriceValidate("100.01")
-		assert.Len(t, errs, 0)
+		validErrs, err := ProductPriceValidate("100.01")
+		assert.NoError(t, err)
+		assert.Len(t, validErrs, 0)
 	})
 
 	t.Run("空文字だった時", func(t *testing.T) {
-		errs := ProductPriceValidate("")
-		assert.Len(t, errs, 1)
-		assert.EqualError(t, errs[0], "空です")
+		validErrs, err := ProductPriceValidate("")
+		assert.NoError(t, err)
+		assert.Len(t, validErrs, 1)
+		assert.EqualError(t, validErrs[0], "空です")
 	})
 
 	t.Run("マイナスだったとき", func(t *testing.T) {
-		errs := ProductPriceValidate("-1")
-		assert.Len(t, errs, 1)
-		assert.EqualError(t, errs[0], "マイナスです")
+		validErrs, err := ProductPriceValidate("-1")
+		assert.NoError(t, err)
+		assert.Len(t, validErrs, 1)
+		assert.EqualError(t, validErrs[0], "マイナスの数値です")
+	})
+
+	t.Run("数値じゃなかったとき", func(t *testing.T) {
+		validErrs, err := ProductPriceValidate("aaa")
+		assert.NoError(t, err)
+		assert.Len(t, validErrs, 1)
+		assert.EqualError(t, validErrs[0], "数値ではありません")
 	})
 }
