8e52670872aa57880160560f75043b40ecfe60d7
mixmaru <mix.touch777@gmail.com>
2020-07-13 22:02:04 +0900

    interface追加

--- internal/domains/contracts/application_service/interfaces/contract_repository_interface.go
+++ internal/domains/contracts/application_service/interfaces/contract_repository_interface.go
@@ -7,4 +7,5 @@ import (
 
 type IContractRepository interface {
 	Create(contractEntity *entities.ContractEntity, executor gorp.SqlExecutor) (savedId int, err error)
+	GetById(id int, executor gorp.SqlExecutor) (*entities.ContractEntity, error)
 }
