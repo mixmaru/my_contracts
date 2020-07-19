7dc4170fd892c8d58c04e386b34d8f5a4ae0811c
mixmaru <mix.touch777@gmail.com>
2020-07-19 12:14:35 +0900

    テスト修正

--- internal/domains/contracts/application_service/contract_application_service_test.go
+++ internal/domains/contracts/application_service/contract_application_service_test.go
@@ -8,63 +8,83 @@ import (
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories"
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/repositories/db_connection"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/gorp.v2"
 	"testing"
 	"time"
 )
 
 func TestContractApplicationService_Register(t *testing.T) {
-	returnContractEntity, err := entities.NewContractEntityWithData(
-		100,
-		2,
-		3,
-		time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-		time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-	)
+	conn, err := db_connection.GetConnection()
+	assert.NoError(t, err)
+	tran, err := conn.Begin()
 	assert.NoError(t, err)
 
-	returnProductEntity, err := entities.NewProductEntityWithData(
-		3,
-		"商品A",
-		"2000",
-		time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-		time.Date(2020, 2, 1, 0, 0, 0, 0, time.UTC),
-	)
+	productRep := repositories.NewProductRepository()
+
+	// productがあればそれを使用する。なければ登録。同名商品は登録できないため
+	product, err := productRep.GetByName("バリデーションテスト商品", tran)
+	assert.NoError(t, err)
+	savedProductId := 0
+	if product == nil {
+		product, err = entities.NewProductEntity("バリデーションテスト商品", "2000")
+		assert.NoError(t, err)
+		savedProductId, err = productRep.Save(product, tran)
+		assert.NoError(t, err)
+	} else {
+		savedProductId = product.Id()
+	}
+
+	// userを新規登録
+	userRep := repositories.NewUserRepository()
+	user, err := entities.NewUserIndividualEntity("個人たろう")
+	assert.NoError(t, err)
+	savedUserId, err := userRep.SaveUserIndividual(user, tran)
 	assert.NoError(t, err)
 
-	returnUserEntity, err := entities.NewUserCorporationEntityWithData(
-		2,
-		"担当太郎",
-		"社長次郎",
-		time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-		time.Date(2020, 2, 1, 0, 0, 0, 0, time.UTC),
-	)
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	contractRepositoryMock := mock_interfaces.NewMockIContractRepository(ctrl)
-	contractRepositoryMock.EXPECT().
-		Create(
-			gomock.AssignableToTypeOf(&entities.ContractEntity{}),
-			gomock.AssignableToTypeOf(&gorp.Transaction{}),
-		).Return(100, nil).
-		Times(1)
-	contractRepositoryMock.EXPECT().
-		GetById(
-			100,
-			gomock.AssignableToTypeOf(&gorp.Transaction{}),
-		).Return(returnContractEntity, returnProductEntity, returnUserEntity, nil).
-		Times(1)
-
-	app := NewContractApplicationServiceWithMock(contractRepositoryMock)
-	dto, _, err := app.Register(2, 3)
+	err = tran.Commit()
 	assert.NoError(t, err)
 
-	assert.Equal(t, 100, dto.Id)
-	assert.Equal(t, 2, dto.UserId)
-	assert.Equal(t, 3, dto.ProductId)
-	assert.True(t, dto.CreatedAt.Equal(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)))
-	assert.True(t, dto.UpdatedAt.Equal(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)))
+	app := NewContractApplicationService()
+
+	t.Run("エラーなし", func(t *testing.T) {
+		dto, validErrors, err := app.Register(savedUserId, savedProductId)
+		assert.NoError(t, err)
+		assert.Len(t, validErrors, 0)
+
+		assert.NotZero(t, dto.Id)
+		assert.Equal(t, savedUserId, dto.UserId)
+		assert.Equal(t, savedProductId, dto.ProductId)
+		assert.NotZero(t, dto.CreatedAt)
+		assert.NotZero(t, dto.UpdatedAt)
+	})
+
+	t.Run("指定されたUserが存在しない", func(t *testing.T) {
+		dto, validationErrors, err := app.Register(-100, savedProductId)
+		assert.NoError(t, err)
+		assert.Len(t, validationErrors, 1)
+		assert.Len(t, validationErrors["user_id"], 1)
+		assert.Equal(t, "存在しません", validationErrors["user_id"][0])
+		assert.Zero(t, dto)
+	})
+
+	t.Run("指定されたProductが存在しない", func(t *testing.T) {
+		dto, validationErrors, err := app.Register(savedUserId, -100)
+		assert.NoError(t, err)
+		assert.Len(t, validationErrors, 1)
+		assert.Len(t, validationErrors["product_id"], 1)
+		assert.Equal(t, "存在しません", validationErrors["product_id"][0])
+		assert.Zero(t, dto)
+	})
+
+	t.Run("指定されたProductもuserも存在しない", func(t *testing.T) {
+		dto, validationErrors, err := app.Register(-1000, -100)
+		assert.NoError(t, err)
+		assert.Len(t, validationErrors, 2)
+		assert.Len(t, validationErrors["user_id"], 1)
+		assert.Equal(t, "存在しません", validationErrors["user_id"][0])
+		assert.Len(t, validationErrors["product_id"], 1)
+		assert.Equal(t, "存在しません", validationErrors["product_id"][0])
+		assert.Zero(t, dto)
+	})
 }
 
 func TestContractApplicationService_GetById(t *testing.T) {
@@ -148,61 +168,5 @@ func TestContractApplicationService_GetById(t *testing.T) {
 		assert.Zero(t, contract)
 		assert.Zero(t, product)
 		assert.Nil(t, user)
-	})
-}
-
-func TestContractApplicationService_registerValidation(t *testing.T) {
-	conn, err := db_connection.GetConnection()
-	assert.NoError(t, err)
-	tran, err := conn.Begin()
-	assert.NoError(t, err)
-
-	productRep := repositories.NewProductRepository()
-
-	// productがあればそれを使用する。なければ登録。同名商品は登録できないため
-	product, err := productRep.GetByName("バリデーションテスト商品", tran)
-	assert.NoError(t, err)
-	savedProductId := 0
-	if product == nil {
-		product, err = entities.NewProductEntity("バリデーションテスト商品", "2000")
-		assert.NoError(t, err)
-		savedProductId, err = productRep.Save(product, tran)
-		assert.NoError(t, err)
-	} else {
-		savedProductId = product.Id()
-	}
-
-	// userを新規登録
-	userRep := repositories.NewUserRepository()
-	user, err := entities.NewUserIndividualEntity("個人たろう")
-	assert.NoError(t, err)
-	savedUserId, err := userRep.SaveUserIndividual(user, tran)
-	assert.NoError(t, err)
-
-	err = tran.Commit()
-	assert.NoError(t, err)
-
-	contractAppService := NewContractApplicationService()
-
-	t.Run("エラーなし", func(t *testing.T) {
-		validationErrors, err := contractAppService.registerValidation(savedUserId, savedProductId, conn)
-		assert.NoError(t, err)
-		assert.Equal(t, map[string][]string{}, validationErrors)
-	})
-
-	t.Run("指定されたUserが存在しない", func(t *testing.T) {
-		validationErrors, err := contractAppService.registerValidation(-100, savedProductId, conn)
-		assert.NoError(t, err)
-		assert.Len(t, validationErrors, 1)
-		assert.Len(t, validationErrors["user_id"], 1)
-		assert.Equal(t, "存在しません", validationErrors["user_id"][0])
-	})
-
-	t.Run("指定されたProductが存在しない", func(t *testing.T) {
-		validationErrors, err := contractAppService.registerValidation(savedUserId, -100, conn)
-		assert.NoError(t, err)
-		assert.Len(t, validationErrors, 1)
-		assert.Len(t, validationErrors["product_id"], 1)
-		assert.Equal(t, "存在しません", validationErrors["product_id"][0])
 	})
 }
