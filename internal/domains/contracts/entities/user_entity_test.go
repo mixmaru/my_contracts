7722b4941a3a693786554a327bab3e4115fe181a
mixmaru <mix.touch777@gmail.com>
2020-07-19 10:52:39 +0900

    UserEntityにDataLoadを実装

---
+++ internal/domains/contracts/entities/user_entity_test.go
@@ -0,0 +1,22 @@
+package entities
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+// インスタンス化された顧客Entityに対してデータロードするやつ
+func TestUserEntity_LoadData(t *testing.T) {
+	user := &UserEntity{}
+	err := user.LoadData(
+		1,
+		time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+		time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+	)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 1, user.Id())
+	assert.Equal(t, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), user.CreatedAt())
+	assert.Equal(t, time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC), user.UpdatedAt())
+}
