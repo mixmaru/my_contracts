c2ad4579fc4a22911748280ec4adfd03537ed79c
mixmaru <mix.touch777@gmail.com>
2020-08-08 20:24:29 +0900

    テーブル定義修正

--- sql_migration/migrations/20200808000000-add_product_monthly_price_tables.sql
+++ sql_migration/migrations/20200808000000-add_product_monthly_price_tables.sql
@@ -4,38 +4,52 @@ create table product_price_monthlies
 	product_id bigint not null
 		constraint product_price_monthlies_products_id_fk
 			references products (id),
+	price numeric not null,
 	created_at timestamptz not null,
 	updated_at timestamptz not null
 );
-insert into product_price_monthlies (product_id, created_at, updated_at) select id, created_at, updated_at from products;
+create unique index product_price_monthlies_product_id_uindex
+	on product_price_monthlies (product_id);
+
+insert into product_price_monthlies (product_id, price, created_at, updated_at) select id, price, created_at, updated_at from products;
+alter table products drop column price;
 
 create table product_price_yearlies
 (
 	product_id bigint not null
 		constraint product_price_yearlies_products_id_fk
 			references products (id),
+	price numeric not null,
 	created_at timestamptz not null,
 	updated_at timestamptz not null
 );
+create unique index product_price_yearlies_product_id_uindex
+	on product_price_yearlies (product_id);
 
 create table product_price_lumps
 (
 	product_id bigint not null
 		constraint product_price_lump_products_id_fk
 			references products (id),
+	price numeric not null,
 	created_at timestamptz not null,
 	updated_at timestamptz not null
 );
+create unique index product_price_lumps_product_id_uindex
+	on product_price_lumps (product_id);
 
 create table product_price_custom_terms
 (
 	product_id bigint not null
-		constraint product_price_lump_products_id_fk
+		constraint product_price_custom_terms_products_id_fk
 			references products (id),
+	price numeric not null,
 	term int not null,
 	created_at timestamptz not null,
 	updated_at timestamptz not null
 );
+create unique index product_price_custom_terms_product_id_uindex
+	on product_price_custom_terms (product_id);
 
 -- +migrate Down
 drop table product_price_monthlies;
