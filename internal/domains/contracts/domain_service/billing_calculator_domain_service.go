a1f3206285db6a6b0451541fe88fd06cdbbefce4
mixmaru <mix.touch777@gmail.com>
2020-08-13 21:31:13 +0900

    リファクタ

--- internal/domains/contracts/domain_service/billing_calculator_domain_service.go
+++ internal/domains/contracts/domain_service/billing_calculator_domain_service.go
@@ -31,26 +31,42 @@ func (b *BillingCalculatorDomainService) BillingAmount(contract *entities.Contra
 	}
 	billingStartDate := contract.BillingStartDate()
 	// 課金開始日から次の課金開始日の間の日数を取得する
-	thisTermDateNum := b.thisTermDateNum(billingStartDate)
+	thisTermFullDateNum := b.billingTermFullDateNum(billingStartDate)
 	// 課金開始日からtargetDateの間の日数を計算する
 	DateNumForTargetDate := int(math.Ceil(targetDate.Sub(billingStartDate).Hours() / 24))
 
-	// 規定日数に足りない場合のみ日割り計算をする（満額時も日割りロジックで算出しようとすると小数点以下の都合で定価がおかしくなる）
-	if DateNumForTargetDate < thisTermDateNum {
-		// 日割り金額を計算する（月額 / 月間日数 * 対象日までの日数）
-		div := price.Div(decimal.NewFromInt(int64(thisTermDateNum)))
-		retPrice := div.Mul(decimal.NewFromInt(int64(DateNumForTargetDate)))
-		return retPrice.Truncate(0), nil
-	} else {
-		// 満額
-		return price, nil
-	}
+	return b.prorate(price, thisTermFullDateNum, DateNumForTargetDate), nil
 }
 
-func (b *BillingCalculatorDomainService) thisTermDateNum(lastBillingStartDate time.Time) int {
+/*
+（1ヶ月契約の）与えられた日を課金開始日として、次の課金開始日までの日数を返す
+
+	billingStartDate	課金開始日
+*/
+func (b *BillingCalculatorDomainService) billingTermFullDateNum(billingStartDate time.Time) int {
 	// 次の課金開始日を算出
-	nextBillingStartDate := lastBillingStartDate.AddDate(0, 1, 0)
+	nextBillingStartDate := billingStartDate.AddDate(0, 1, 0)
 	// 日数算出
-	oneMonthDuration := nextBillingStartDate.Sub(lastBillingStartDate)
+	oneMonthDuration := nextBillingStartDate.Sub(billingStartDate)
 	return int(math.Ceil(oneMonthDuration.Hours() / 24))
+}
+
+/*
+日割り計算
+
+	basePrice 	定価
+	fullDateNum 基底日数
+	usedDateNum 使用日
+*/
+func (b *BillingCalculatorDomainService) prorate(basePrice decimal.Decimal, fullDateNum int, usedDateNum int) decimal.Decimal {
+	// 規定日数に足りない場合のみ日割り計算をする（満額時も日割りロジックで算出しようとすると小数点以下の都合で定価がおかしくなる）
+	if usedDateNum >= fullDateNum {
+		// 満額
+		return basePrice
+	}
+
+	// 日割り金額を計算する（月額 / 月間日数 * 対象日までの日数）
+	div := basePrice.Div(decimal.NewFromInt(int64(fullDateNum)))
+	retPrice := div.Mul(decimal.NewFromInt(int64(usedDateNum)))
+	return retPrice.Truncate(0)
 }
