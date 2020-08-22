948d76039cba45c97c1dcccaae89466163c26b21
mixmaru <mix.touch777@gmail.com>
2020-08-22 21:50:39 +0900

    テストリファクタ

--- internal/domains/contracts/application_service/user_application_service_test.go
+++ internal/domains/contracts/application_service/user_application_service_test.go
@@ -1,67 +1,25 @@
 package application_service
 
 import (
-	"github.com/golang/mock/gomock"
 	"github.com/mixmaru/my_contracts/internal/domains/contracts/application_service/data_transfer_objects"
-	"github.com/mixmaru/my_contracts/internal/domains/contracts/application_service/interfaces/mock_interfaces"
-	"github.com/mixmaru/my_contracts/internal/domains/contracts/entities"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/gorp.v2"
 	"testing"
-	"time"
 )
 
 // 個人顧客情報の登録とデータ取得のテスト
 func TestUserApplicationService_RegisterUserIndividual(t *testing.T) {
-	t.Run("正常系", func(t *testing.T) {
-		// リポジトリのSaveUserIndividual()が受け取る引数を用意
-		saveUserEntity, err := entities.NewUserIndividualEntity("個人太郎")
-		assert.NoError(t, err)
-
-		now := time.Now()
-		returnUserEntity, err := entities.NewUserIndividualEntity("既存太郎")
-		assert.NoError(t, err)
-		err = returnUserEntity.LoadData(1, "個人太郎", now, now)
-		assert.NoError(t, err)
-
-		// モックリポジトリ作成
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
-		userRepositoryMock.EXPECT().
-			SaveUserIndividual(
-				saveUserEntity,
-				gomock.AssignableToTypeOf(&gorp.Transaction{}),
-			).Return(1, nil).
-			Times(1)
-		userRepositoryMock.EXPECT().
-			GetUserIndividualById(
-				1,
-				gomock.AssignableToTypeOf(&gorp.Transaction{}),
-			).Return(returnUserEntity, nil).
-			Times(1)
-
-		// インスタンス化
-		userApp := NewUserApplicationServiceWithMock(userRepositoryMock)
-
-		registerdUser, validErrs, err := userApp.RegisterUserIndividual("個人太郎")
+	userApp := NewUserApplicationService()
+	t.Run("名前を渡すと個人顧客データを登録できる", func(t *testing.T) {
+		registeredUser, validErrs, err := userApp.RegisterUserIndividual("個人太郎")
 		assert.Len(t, validErrs, 0)
 		assert.NoError(t, err)
-		assert.Equal(t, 1, registerdUser.Id)
-		assert.Equal(t, "個人太郎", registerdUser.Name)
-		assert.Equal(t, now, registerdUser.CreatedAt)
-		assert.Equal(t, now, registerdUser.UpdatedAt)
+		assert.NotZero(t, registeredUser.Id)
+		assert.Equal(t, "個人太郎", registeredUser.Name)
+		assert.NotZero(t, registeredUser.CreatedAt)
+		assert.NotZero(t, registeredUser.UpdatedAt)
 	})
 
-	t.Run("バリデーションエラー　名前がから文字", func(t *testing.T) {
-		// モックリポジトリ作成
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
-
-		// インスタンス化
-		userApp := NewUserApplicationServiceWithMock(userRepositoryMock)
-
+	t.Run("バリデーションエラー　名前がから文字のときvalidErrsが返る", func(t *testing.T) {
 		_, validErrs, err := userApp.RegisterUserIndividual("")
 		assert.NoError(t, err)
 
@@ -74,15 +32,7 @@ func TestUserApplicationService_RegisterUserIndividual(t *testing.T) {
 		assert.Equal(t, expectValidErrs, validErrs)
 	})
 
-	t.Run("バリデーションエラー　名前が50文字以上", func(t *testing.T) {
-		// モックリポジトリ作成
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
-
-		// インスタンス化
-		userApp := NewUserApplicationServiceWithMock(userRepositoryMock)
-
+	t.Run("バリデーションエラー　名前が50文字以上のときvalidErrorが返る", func(t *testing.T) {
 		_, validErrs, err := userApp.RegisterUserIndividual("000000000011111111112222222222333333333344444444445")
 		assert.NoError(t, err)
 		expectValidErrs := map[string][]string{
@@ -96,51 +46,21 @@ func TestUserApplicationService_RegisterUserIndividual(t *testing.T) {
 }
 
 func TestUserApplicationService_RegisterUserCorporation(t *testing.T) {
-	t.Run("正常系", func(t *testing.T) {
-		now := time.Now()
-		returnUserEntity, err := entities.NewUserCorporationEntityWithData(1, "イケてる会社", "担当太郎", "社長太郎", now, now)
-		assert.NoError(t, err)
-
-		// モックリポジトリ作成
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
-		userRepositoryMock.EXPECT().
-			SaveUserCorporation(
-				gomock.Any(),
-				gomock.AssignableToTypeOf(&gorp.Transaction{}),
-			).Return(1, nil).
-			Times(1)
-		userRepositoryMock.EXPECT().
-			GetUserCorporationById(
-				1,
-				gomock.AssignableToTypeOf(&gorp.Transaction{}),
-			).Return(returnUserEntity, nil).
-			Times(1)
-
-		// インスタンス化
-		userApp := NewUserApplicationServiceWithMock(userRepositoryMock)
-
+	userApp := NewUserApplicationService()
+	t.Run("会社名と担当者名と社長名を渡すと法人顧客データが登録できる", func(t *testing.T) {
 		registeredUser, validErrs, err := userApp.RegisterUserCorporation("イケてる会社", "担当太郎", "社長太郎")
 		assert.NoError(t, err)
 		assert.Len(t, validErrs, 0)
-		assert.Equal(t, 1, registeredUser.Id)
+
+		assert.NotZero(t, registeredUser.Id)
 		assert.Equal(t, "イケてる会社", registeredUser.CorporationName)
 		assert.Equal(t, "担当太郎", registeredUser.ContactPersonName)
 		assert.Equal(t, "社長太郎", registeredUser.PresidentName)
-		assert.Equal(t, now, registeredUser.CreatedAt)
-		assert.Equal(t, now, registeredUser.UpdatedAt)
+		assert.NotZero(t, registeredUser.CreatedAt)
+		assert.NotZero(t, registeredUser.UpdatedAt)
 	})
 
-	t.Run("バリデーションエラー　会社名と担当者名と社長名がから文字", func(t *testing.T) {
-		// モックリポジトリ作成
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
-
-		// インスタンス化
-		userApp := NewUserApplicationServiceWithMock(userRepositoryMock)
-
+	t.Run("バリデーションエラー　会社名と担当者名と社長名がから文字だったらバリデーションエラーが返る", func(t *testing.T) {
 		_, validErrs, err := userApp.RegisterUserCorporation("", "", "")
 		assert.NoError(t, err)
 		expectValidErrs := map[string][]string{
@@ -158,15 +78,7 @@ func TestUserApplicationService_RegisterUserCorporation(t *testing.T) {
 		assert.Equal(t, expectValidErrs, validErrs)
 	})
 
-	t.Run("バリデーションエラー　名前が50文字以上", func(t *testing.T) {
-		// モックリポジトリ作成
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		userRepositoryMock := mock_interfaces.NewMockIUserRepository(ctrl)
-
-		// インスタンス化
-		userApp := NewUserApplicationServiceWithMock(userRepositoryMock)
-
+	t.Run("バリデーションエラー　名前が50文字以上だったらバリデーションエラーメッセージが返る", func(t *testing.T) {
 		_, validErrs, err := userApp.RegisterUserCorporation("000000000011111111112222222222333333333344444444445", "000000000011111111112222222222333333333344444444445", "000000000011111111112222222222333333333344444444445")
 		assert.NoError(t, err)
 		expectValidErrs := map[string][]string{
@@ -196,7 +108,7 @@ func TestUserApplicationService_GetUserById(t *testing.T) {
 	assert.Len(t, validErrors, 0)
 
 	t.Run("個人顧客", func(t *testing.T) {
-		t.Run("データがある時", func(t *testing.T) {
+		t.Run("データがある時はidでデータ取得ができる", func(t *testing.T) {
 			user, err := app.GetUserById(individualDto.Id)
 			assert.NoError(t, err)
 			userDto, ok := user.(data_transfer_objects.UserIndividualDto)
@@ -207,7 +119,7 @@ func TestUserApplicationService_GetUserById(t *testing.T) {
 			assert.NotZero(t, userDto.UpdatedAt)
 		})
 
-		t.Run("データが無いとき", func(t *testing.T) {
+		t.Run("データが無いときはnilが返る", func(t *testing.T) {
 			user, err := app.GetUserById(-100)
 			assert.NoError(t, err)
 			assert.Nil(t, user)
@@ -215,7 +127,7 @@ func TestUserApplicationService_GetUserById(t *testing.T) {
 	})
 
 	t.Run("法人顧客", func(t *testing.T) {
-		t.Run("データがある時", func(t *testing.T) {
+		t.Run("データがある時はidでデータが取得できる", func(t *testing.T) {
 			user, err := app.GetUserById(corporationDto.Id)
 			assert.NoError(t, err)
 			userDto, ok := user.(data_transfer_objects.UserCorporationDto)
@@ -226,10 +138,9 @@ func TestUserApplicationService_GetUserById(t *testing.T) {
 			assert.Equal(t, "法人顧客取得テスト社長", userDto.PresidentName)
 			assert.NotZero(t, userDto.CreatedAt)
 			assert.NotZero(t, userDto.UpdatedAt)
-
 		})
 
-		t.Run("データが無いとき", func(t *testing.T) {
+		t.Run("データが無いときはnilが返る", func(t *testing.T) {
 			user, err := app.GetUserById(-100)
 			assert.NoError(t, err)
 			assert.Nil(t, user)
