ab3bc55a1221671cf2ecb75f0b1617efbffd5a87
mixmaru <mix.touch777@gmail.com>
2020-09-26 22:56:45 +0900

    コード移動

--- api/main.go
+++ api/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/mixmaru/my_contracts/internal/utils"
 	"github.com/mixmaru/my_contracts/internal/utils/my_logger"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -43,68 +42,6 @@ func newRouter() *echo.Echo {
 	e.POST("/batches/bills/billing", executeBilling)
 
 	return e
-}
-
-// 商品新規登録
-// params:
-// name string 商品名
-// price string 価格
-// curl -F "name=A商品" -F "price=10.1" http://localhost:1323/individual_users
-func saveProduct(c echo.Context) error {
-	logger, err := my_logger.GetLogger()
-	if err != nil {
-		return err
-	}
-
-	// Get name and email
-	name := c.FormValue("name")
-	price := c.FormValue("price")
-
-	productAppService := application_service.NewProductApplicationService()
-	product, validErrs, err := productAppService.Register(name, price)
-	if err != nil {
-		logger.Sugar().Errorw("商品データ登録に失敗。", "name", name, "price", price, "err", err)
-		c.Error(err)
-		return err
-	}
-	if len(validErrs) > 0 {
-		return c.JSON(http.StatusBadRequest, validErrs)
-	}
-
-	return c.JSON(http.StatusCreated, product)
-}
-
-// 商品情報取得
-// curl http://localhost:1323/products/1
-func getProduct(c echo.Context) error {
-	logger, err := my_logger.GetLogger()
-	if err != nil {
-		return err
-	}
-
-	productId, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		// idに変な値が渡された
-		return c.JSON(http.StatusNotFound, echo.ErrNotFound)
-	}
-
-	// サービスインスタンス化
-	productAppService := application_service.NewProductApplicationService()
-	// データ取得
-	product, err := productAppService.Get(productId)
-	if err != nil {
-		logger.Sugar().Errorw("商品データ取得に失敗。", "productId", productId, "err", err)
-		c.Error(err)
-		return err
-	}
-
-	// データがない
-	if product.Id == 0 {
-		return c.JSON(http.StatusNotFound, echo.ErrNotFound)
-	}
-
-	// 返却
-	return c.JSON(http.StatusOK, product)
 }
 
 // 請求実行バッチ
