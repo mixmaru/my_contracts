780b528798edeaeaf1efe2c4bff334c1213aac74
mixmaru <mix.touch777@gmail.com>
2020-12-12 09:48:53 +0900

    カスタマープロパティタイプのユースケース作成

---
+++ core/application/customer_property_type/create/i_customer_type_create_use_case.go
@@ -0,0 +1,31 @@
+package create
+
+import (
+	products2 "github.com/mixmaru/my_contracts/core/application/customer_property_type"
+	"github.com/pkg/errors"
+)
+
+type ICustomerPropertyTypeCreateUseCase interface {
+	Handle(request *CustomerPropertyTypeCreateUseCaseRequest) (*CustomerPropertyTypeCreateUseCaseResponse, error)
+}
+
+type CustomerPropertyTypeCreateUseCaseRequest struct {
+	Name string
+	Type string
+}
+
+func NewCustomerPropertyTypeCreateUseCaseRequest(name string, propertyType string) (*CustomerPropertyTypeCreateUseCaseRequest, error) {
+	if propertyType != "string" && propertyType != "numeric" {
+		return nil, errors.Errorf("propertyTypeがstringでもnumericでもありません。propertyType: %v", propertyType)
+	}
+	return &CustomerPropertyTypeCreateUseCaseRequest{Name: name, Type: propertyType}, nil
+}
+
+type CustomerPropertyTypeCreateUseCaseResponse struct {
+	CustomerPropertyTypeDto products2.CustomerPropertyTypeDto
+	ValidationError         map[string][]string
+}
+
+func NewCustomerPropertyTypeCreateUseCaseResponse(customerPropertyTypeDto products2.CustomerPropertyTypeDto, validationError map[string][]string) *CustomerPropertyTypeCreateUseCaseResponse {
+	return &CustomerPropertyTypeCreateUseCaseResponse{CustomerPropertyTypeDto: customerPropertyTypeDto, ValidationError: validationError}
+}
