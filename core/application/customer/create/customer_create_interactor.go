90b0b3bf1c3463a105325d6d0c61308c30009287
mixmaru <mix.touch777@gmail.com>
2021-01-07 08:50:41 +0900

    interactor実装中

---
+++ core/application/customer/create/customer_create_interactor.go
@@ -0,0 +1,27 @@
+package create
+
+import "github.com/mixmaru/my_contracts/core/domain/models/customer"
+
+type CustomerCreateInteractor struct {
+}
+
+func NewCustomerCreateInteractor() *CustomerCreateInteractor {
+	return &CustomerCreateInteractor{}
+}
+
+func (c CustomerCreateInteractor) Handle(request *CustomerCreateUseCaseRequest) (*CustomerCreateUseCaseResponse, error) {
+	response := CustomerCreateUseCaseResponse{}
+
+	// バリデーションする
+
+	// entityをつくる
+	newEntity := customer.NewCustomerEntity(request.Name, request.Properties)
+
+	// repositoryで保存する
+
+	// 再読込する
+
+	// dtoに詰める
+
+	return &response, nil
+}
