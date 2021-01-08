b83d9befaacf3112c3cee7882976c841b7b0a209
mixmaru <mix.touch777@gmail.com>
2021-01-08 23:27:41 +0900

    customerEntityにプロパティ追加

--- core/domain/models/customer/customer_entity.go
+++ core/domain/models/customer/customer_entity.go
@@ -1,24 +1,30 @@
 package customer
 
 type CustomerEntity struct {
-	id         int
-	name       string
-	properties map[string]interface{}
+	id             int
+	name           string
+	customerTypeId int
+	properties     map[string]interface{}
 }
 
-func NewCustomerEntity(name string, properties map[string]interface{}) *CustomerEntity {
-	return &CustomerEntity{name: name, properties: properties}
+func NewCustomerEntity(name string, customerTypeId int, properties map[string]interface{}) *CustomerEntity {
+	return &CustomerEntity{
+		name:           name,
+		customerTypeId: customerTypeId,
+		properties:     properties,
+	}
 }
 
-func NewCustomerEntityWithData(id int, name string, properties map[string]interface{}) *CustomerEntity {
+func NewCustomerEntityWithData(id int, name string, customerTypeId int, properties map[string]interface{}) *CustomerEntity {
 	entity := CustomerEntity{}
-	entity.LoadData(id, name, properties)
+	entity.LoadData(id, name, customerTypeId, properties)
 	return &entity
 }
 
-func (c *CustomerEntity) LoadData(id int, name string, properties map[string]interface{}) {
+func (c *CustomerEntity) LoadData(id int, name string, customerTypeId int, properties map[string]interface{}) {
 	c.id = id
 	c.name = name
+	c.customerTypeId = customerTypeId
 	c.properties = properties
 }
 
@@ -28,6 +34,10 @@ func (c *CustomerEntity) Id() int {
 
 func (c *CustomerEntity) Name() string {
 	return c.name
+}
+
+func (c *CustomerEntity) CustomerTypeId() int {
+	return c.customerTypeId
 }
 
 func (c *CustomerEntity) Properties() map[string]interface{} {
