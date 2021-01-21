66fed132ff585ddb9486f8439e84d26503af3bdd
mixmaru <mix.touch777@gmail.com>
2021-01-21 11:49:20 +0900

    test用utilsを作った

--- core/application/customer/get_by_id/customer_get_by_id_interactor_test.go
+++ core/application/customer/get_by_id/customer_get_by_id_interactor_test.go
@@ -1,15 +1,7 @@
 package get_by_id
 
 import (
-	"github.com/mixmaru/my_contracts/core/application/customer"
-	"github.com/mixmaru/my_contracts/core/application/customer/create"
-	"github.com/mixmaru/my_contracts/core/application/customer_property_type"
-	create2 "github.com/mixmaru/my_contracts/core/application/customer_property_type/create"
-	"github.com/mixmaru/my_contracts/core/application/customer_type"
-	create3 "github.com/mixmaru/my_contracts/core/application/customer_type/create"
-	"github.com/mixmaru/my_contracts/core/infrastructure/db"
-	"github.com/mixmaru/my_contracts/utils"
-	"github.com/pkg/errors"
+	"github.com/mixmaru/my_contracts/test_utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -17,7 +9,7 @@ import (
 func TestCustomerGetByIdInteractor_Handle(t *testing.T) {
 	////// 事前準備
 	// カスタマー登録
-	customerDto, err := preCreateCustomer()
+	customerDto, err := test_utils.PreCreateCustomer()
 	assert.NoError(t, err)
 	assert.NotZero(t, customerDto)
 
@@ -28,64 +20,4 @@ func TestCustomerGetByIdInteractor_Handle(t *testing.T) {
 	t.Run("存在しないCustomerIdを渡すとゼロ値が返る", func(t *testing.T) {
 
 	})
-}
-
-func preCreateCustomer() (customer.CustomerDto, error) {
-	timestampStr := utils.CreateTimestampString()
-	propertyDto1, err := preCreateCustomerPropertyType("性別"+timestampStr, "string")
-	if err != nil {
-		return customer.CustomerDto{}, err
-	}
-	propertyDto2, err := preCreateCustomerPropertyType("年齢"+timestampStr, "numeric")
-	if err != nil {
-		return customer.CustomerDto{}, err
-	}
-	customerTypeDto, err := preCreateCustomerType("顧客タイプ"+timestampStr, []int{propertyDto1.Id, propertyDto2.Id})
-	if err != nil {
-		return customer.CustomerDto{}, err
-	}
-
-	request := create.NewCustomerCreateUseCaseRequest(
-		"顧客名"+timestampStr,
-		customerTypeDto.Id,
-		map[int]interface{}{
-			propertyDto1.Id: "女",
-			propertyDto2.Id: 25.,
-		},
-	)
-	interactor := create.NewCustomerCreateInteractor(db.NewCustomerRepository())
-	response, err := interactor.Handle(request)
-	if err != nil {
-		return customer.CustomerDto{}, err
-	}
-	if len(response.ValidationErrors) > 0 {
-		return customer.CustomerDto{}, errors.Errorf("バリデーションエラー。%+v", response.ValidationErrors)
-	}
-	return response.CustomerDto, nil
-}
-
-func preCreateCustomerPropertyType(propertyName string, propertyType string) (customer_property_type.CustomerPropertyTypeDto, error) {
-	request := create2.NewCustomerPropertyTypeCreateUseCaseRequest(propertyName, propertyType)
-	interactor := create2.NewCustomerPropertyTypeCreateInteractor(db.NewCustomerPropertyTypeRepository())
-	response, err := interactor.Handle(request)
-	if err != nil {
-		return customer_property_type.CustomerPropertyTypeDto{}, err
-	}
-	if len(response.ValidationErrors) > 0 {
-		return customer_property_type.CustomerPropertyTypeDto{}, errors.Errorf("バリデーションエラー。%+v", response.ValidationErrors)
-	}
-	return response.CustomerPropertyTypeDto, nil
-}
-
-func preCreateCustomerType(customerTypeName string, propertyTypeIds []int) (customer_type.CustomerTypeDto, error) {
-	request := create3.NewCustomerTypeCreateUseCaseRequest(customerTypeName, propertyTypeIds)
-	interactor := create3.NewCustomerTypeCreateInteractor(db.NewCustomerTypeRepository(), db.NewCustomerPropertyTypeRepository())
-	response, err := interactor.Handle(request)
-	if err != nil {
-		return customer_type.CustomerTypeDto{}, err
-	}
-	if len(response.ValidationErrors) > 0 {
-		return customer_type.CustomerTypeDto{}, errors.Errorf("バリデーションエラー。%+v", response.ValidationErrors)
-	}
-	return response.CustomerTypeDto, nil
 }
